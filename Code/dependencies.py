b2e1426ac6ce7a6b72d6dfc320d6e3deb15de3f6
m0jibake <62702300+m0jibake@users.noreply.github.com>
2020-04-02 21:12:20 +0200

    Add dependencies.py file

---
+++ Code/dependencies.py
@@ -0,0 +1,2 @@
+# This is an auxiliary file to import python modules and packages into a jupyter notebook
+# Calling this file by '%run ./dependencies' in the jupyter notebook does the respective job
