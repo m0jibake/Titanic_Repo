b0e414a9bf48fb81e12321cbdb45ffa46679acf0
m0jibake <62702300+m0jibake@users.noreply.github.com>
2020-06-24 13:43:31 +0200

    Update dependencies.py

--- Code/dependencies.py
+++ Code/dependencies.py
@@ -1,2 +1,24 @@
 # This is an auxiliary file to import python modules and packages into a jupyter notebook
 # Calling this file by '%run ./dependencies' in the jupyter notebook does the respective job
+
+import os
+import pandas as pd
+import numpy as np
+import random
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from pandas.plotting import scatter_matrix
+
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import LabelEncoder
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import cross_val_score
+
+from jupyterthemes import jtplot
+jtplot.style(theme='chesterish', context='notebook', ticks=True, grid=False)
+
+from IPython.core.interactiveshell import InteractiveShell
+InteractiveShell.ast_node_interactivity = "all"
