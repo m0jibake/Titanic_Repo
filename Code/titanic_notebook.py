131d82a856dc573e56dd65864a76dd2c08abf36c
7tHeaven <fz1993@gmx.de>
2020-07-30 11:43:33 +0200

    Encoded Sex and Embarked, replace Nan in Age

--- Code/titanic_notebook.py
+++ Code/titanic_notebook.py
@@ -5,7 +5,7 @@
 #       extension: .py
 #       format_name: light
 #       format_version: '1.5'
-#       jupytext_version: 1.4.1
+#       jupytext_version: 1.5.2
 #   kernelspec:
 #     display_name: Python 3
 #     language: python
@@ -18,7 +18,7 @@
 
 pd.set_option('display.max_columns', 500)
 
-repo_directory = Path('D:\Kaggle\Titanic\Titanic_Repo')
+repo_directory = Path('D:\Kaggle\Titanic_Repo')
 code_path = repo_directory / 'Code'
 data_path = repo_directory / 'Data'
 
@@ -43,12 +43,32 @@ print('The test data contains {0} columns and {1} rows'.format(df_test.shape[1],
 #
 # The following utility functions may be applied: 
 # encode_categorical_vars() --- to transform categorical variables to numerical ones via the dummy variable approach
+# -
+
+#Encode Sex and Embarked variables
+df_train = encode_categorical_vars(df_train, ['Sex', 'Embarked'])
+#Set mean for 'NaN' fields in Age
+df_train['Age'].fillna(df_train['Age'].mean(), inplace = True)
+
+#mutiplied Age gives different accuracy
+df_train['Age_multiplied'] = df_train['Age']/1000
+
+df_train.head(10)
 
 # + active=""
 # All pre-processing done? Then proceed with the training procedure
 #
 # Sample usage (copy the following line in a new cell):
 # training_example = training_proc(df_train,['Survived', 'Pclass', 'Fare'])
+# -
+
+#Standard train
+training_example = training_proc(df_train,['Survived', 'Pclass', 'Age','Fare', 'Sex_female', 'Sex_male', 
+                                           'Embarked_C', 'Embarked_Q', 'Embarked_S'])
+
+#Experimental train with multiplied Age
+training_example = training_proc(df_train,['Survived', 'Pclass', 'Age_multiplied','Fare', 
+                                           'Sex_female', 'Sex_male', 'Embarked_C', 'Embarked_Q', 'Embarked_S'])
 
 # + active=""
 # Apply the fitted estimators to the test data
